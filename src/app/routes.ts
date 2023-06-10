55af4bcd5060150590ba6d282c8829f958e606ce
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-06-10 01:00:26 +0100

    Adding routing

---
+++ src/app/routes.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+
+const routeConfig: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home page'
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Home details'
+    }
+];
+
+
+
+export default routeConfig;
